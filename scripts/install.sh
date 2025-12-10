04691915c54cc2a35feacda2a33eea5486e75735
GuillaumeCa <guicarre1@gmail.com>
2025-12-10 22:26:09 +0000

    fix install script for diff

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-BRANCH=main
+REF=46eff52740fe4485cae229f03de5c261627565bd
 
 if [ ! -d "Horizon" ]; then
-    echo "📦 Fetching Horizon repository with branch: $BRANCH"
+    echo "📦 Fetching Horizon repository with ref: $REF"
     git clone https://github.com/Far-Beyond-Dev/Horizon.git
 fi
 
 cd Horizon
 git fetch
-git checkout $BRANCH
-git pull
+git checkout $REF
+git pull origin $REF
+
+git apply ../horizon.diff
